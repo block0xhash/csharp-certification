0f8271d40e3a10d1b2b3d779f3ce755e9ebf671d
block0xhash <bothwellw@gmail.com>
2025-05-18 13:43:05 +0200

    literal values

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//1. Write your first program using c#
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Congratulations");
+Console.WriteLine("It works");
+
+//2. https://learn.microsoft.com/training/modules/csharp-literals-variables/ 
+
+Console.WriteLine('b');
+
+//Console.WriteLine('Hello World'); //compiler expects a single character when single qoutes are used
+Console.WriteLine(123);
+Console.WriteLine(0.25F);     // float 32 bits(4bytes)
+Console.WriteLine(2.625);     // double 64 bits (8 bytes)
+Console.WriteLine(12.39816m); // decimal 128 bits (64 bytes )
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+
+
